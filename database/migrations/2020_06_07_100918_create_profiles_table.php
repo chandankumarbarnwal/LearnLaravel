61076f01762e15eb0c23b3d4d9ebd1831fa7a51e
chandankumarbarnwal <chandanbarnwal111@gmail.com>
2020-06-07 18:56:54 +0530

    Learnt one to one eloquent relations

--- database/migrations/2020_06_07_100918_create_profiles_table.php
+++ database/migrations/2020_06_07_100918_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+        
+            $table->unsignedBigInteger('author_id')->unique();
+            $table->foreign('author_id')->references('id')->on('authors');
         });
     }
 
@@ -31,6 +29,11 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropForeign('profiles_author_id_foreign');
+
+        });
+
+        Schema::dropIfExists('profiles');
     }
 }
